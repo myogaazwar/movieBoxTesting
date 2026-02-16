efd4efc8fb1b458513481e0c0dc16da2e7f45721
myogaazwar <yogaazwar123@gmail.com>
2026-02-16 23:55:17 +0700

    create login page for web element and function testing login

---
+++ src/main/java/com/moviebox/pages/LoginPage.java
@@ -0,0 +1,51 @@
+package com.moviebox.pages;
+
+import com.base.BasePage;
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+    private final By descriptionLoginPage = By.cssSelector("body > main > section > div > div.text-center > h2");
+    private final By inputUsername = By.name("username");
+    private final By inputPassword = By.name("password");
+    private final By buttonCurrentSwitchLanguage = By.cssSelector("body > div > button");
+    private final By buttonChoosenSwitchLanguage = By.cssSelector("body > div > div > a");
+    private final By buttonLogin = By.cssSelector("body > main > section > div > form > button");
+    private final By messageErrorUsername = By.xpath("//input[@name='username']/following-sibling::small");
+    private final By messageErrorPassword = By.xpath("//input[@name='password']/following-sibling::small");
+
+
+    public String readingDescriptioPage () {
+        return find(descriptionLoginPage).getText();
+    }
+
+    public void changeLanguage() {
+        find(buttonCurrentSwitchLanguage).click();
+        find(buttonChoosenSwitchLanguage).click();
+    }
+
+    public void sendInputLogin(String username, String password) {
+            set(inputUsername, username);
+            set(inputPassword, password);
+    }
+
+    public void clickButtonLogin() {
+        click(buttonLogin);
+    }
+
+    public String readingErrorMessageFieldUsername() {
+        return find(messageErrorUsername).getText();
+    }
+
+    public String readingErrorMessageFieldPassword () {
+        return find(messageErrorPassword).getText();
+    }
+
+    public MoviesPage logIntoApplication (String username, String password) {
+        sendInputLogin(username, password);
+        clickButtonLogin();
+
+        return new MoviesPage();
+    }
+
+
+}
