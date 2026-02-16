d8a08bf81d3b2648663ee4e39f7c05cac4ac18dd
myogaazwar <yogaazwar123@gmail.com>
2026-02-16 23:54:37 +0700

    Create Base Page for setup testing

---
+++ src/main/java/com/base/BasePage.java
@@ -0,0 +1,38 @@
+package com.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+
+public class BasePage {
+    public static WebDriver driver;
+    public static Wait<WebDriver> wait;
+
+    public void setDriver(WebDriver driver, Wait<WebDriver> wait) {
+        BasePage.driver = driver;
+        BasePage.wait = wait;
+    }
+
+    protected WebElement find(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void set(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        find(locator).click();
+    }
+
+    public static void delay(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
